075ac0e27a17c8be1b153fb9ccac56b4b3ec3dce
YOUR NAME <dustyn.august@gmail.com>
2017-04-12 20:27:17 -0700

    Finished first working version of messaging.

---
+++ roomy/Pods/Target Support Files/ParseLiveQuery/ParseLiveQuery-umbrella.h
@@ -0,0 +1,11 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+#import "ParseLiveQuery-Bridging-Header.h"
+#import "PFDecoder_internal.h"
+#import "PFEncoder_internal.h"
+
+FOUNDATION_EXPORT double ParseLiveQueryVersionNumber;
+FOUNDATION_EXPORT const unsigned char ParseLiveQueryVersionString[];
+
